ed6065fe552277e9e995df255c626ac418cb212c
sudhirrajai <hackersmania125@gmail.com>
2026-05-09 17:08:52 +0530

    small fix

--- resources/js/Pages/Domains/Index.vue
+++ resources/js/Pages/Domains/Index.vue
@@ -32,6 +32,7 @@
       <!-- Domain Table -->
       <div class="row">
         <div class="col-12">
+          <div class="card">
             <div class="card-header pb-0 d-flex justify-content-between align-items-center">
               <h6 class="mb-0">Your Domains</h6>
               <div class="ms-md-auto pe-md-3 d-flex align-items-center">
